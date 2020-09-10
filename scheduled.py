d4afbb2701329818c45b6fd85696414194f91563
Yusuf Yildiz <yusuf_yildiz@student.uml.edu>
2020-09-10 00:02:07 -0400

    uses scheduler

--- scheduled.py
+++ scheduled.py
@@ -1,3 +1,16 @@
 import update
 
-update.updateHtaccess()
+# put backin Procfile: web: gunicorn app:app --log-file=-
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+def disable_interval():
+    sched.remove_job('INTERVAL_JOB')
+
+def enable_interval():
+    sched.add_job(update.updateHtaccess(), 'interval', minutes=1, id='INTERVAL_JOB')
+
+sched.start()
+sched.add_job(enable_interval, 'cron', day_of_week='thu')
+sched.add_job(disable_interval, 'cron', day_of_week='sat')
